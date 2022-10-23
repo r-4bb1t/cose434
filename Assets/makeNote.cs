4025abf18a05a5fd26dd140b3e44af0dfe3a7aff
r4bb1t <r4bb1t.github@gmail.com>
2022-10-24 01:54:22 +0900

    add long note

--- Assets/makeNote.cs
+++ Assets/makeNote.cs
@@ -1,28 +1,131 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[System.Serializable]
+public class Note
+{
+    public int noteType;
+}
+
+[System.Serializable]
+public class Beat
+{
+    public Note[] notes;
+}
+
+[System.Serializable]
+public class Bar
+{
+    public Beat[] beats;
+}
+
+[System.Serializable]
+public class Chart
+{
+    public string title;
+    public int level;
+    public string composer;
+    public string cover;
+    public int bpm;
+    public int barNumber;
+    public Bar[] bars;
+}
+
+[RequireComponent(typeof(AudioSource))]
 public class makeNote : MonoBehaviour
 {
-    public GameObject noteParent;
+    public GameObject defaultNote;
+    public GameObject longNote;
+    public GameObject startNote;
+    public GameObject endNote;
+    public GameObject UI;
     NoteParent script;
+    Chart data;
+    int n;
+    float deltaTime = 0.0f;
+    private AudioSource audioSource;
 
     void MakeNote()
     {
-        GameObject np = Instantiate(noteParent);
-        np.transform.position = new Vector3(-10.85f, 2f, -5.409f);
+        for (int i = 0; i < 8; i++)
+        {
+            Note note = data.bars[0].beats[n].notes[i];
+            if (note.noteType == 1)
+            {
+                GameObject dn = Instantiate(defaultNote);
+                dn.transform.SetParent(UI.transform);
+                dn.transform.localPosition = Vector3.zero;
 
-        np.GetComponent<NoteParent>().dir = Random.Range(0, 8) * 45;
+                dn.GetComponent<NoteParent>().dir = i * 45;
+                Destroy(dn, 2f);
+            }
+            if (note.noteType == 2)
+            {
+                GameObject sn = Instantiate(startNote);
+                sn.transform.SetParent(UI.transform);
+                sn.transform.localPosition = Vector3.zero;
+
+                sn.GetComponent<NoteParent>().dir = i * 45;
+                Destroy(sn, 2f);
+            }
+            if (note.noteType == 3)
+            {
+                GameObject ln = Instantiate(longNote);
+                ln.transform.SetParent(UI.transform);
+                ln.transform.localPosition = Vector3.zero;
+
+                ln.GetComponent<NoteParent>().dir = i * 45;
+                Destroy(ln, 2f);
+            }
+            if (note.noteType == 4)
+            {
+                GameObject en = Instantiate(endNote);
+                en.transform.SetParent(UI.transform);
+                en.transform.localPosition = Vector3.zero;
+
+                en.GetComponent<NoteParent>().dir = i * 45;
+                Destroy(en, 2f);
+            }
+        }
+        n++;
     }
-    
+
+    void SetUI()
+    {
+        UI.transform.Find("Title").GetComponent<TextMeshProUGUI>().text = data.title;
+        UI.transform.Find("Composer").GetComponent<TextMeshProUGUI>().text = data.composer;
+        UI.transform.Find("Level").GetComponent<TextMeshProUGUI>().text = "LV " + data.level;
+    }
+
+    void setBgm()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        AudioClip audioAsset = (AudioClip)Resources.Load("Audio/emocloche");
+        audioSource.clip = (AudioClip)audioAsset;
+        audioSource.Play();
+    }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 40;
+    }
+
     void Start()
     {
-        InvokeRepeating("MakeNote", 2f, 2f); // 2초뒤 0.3초주기로 ExampleInvokeUse함수 반복 호출
+        var loadedJson = Resources.Load("JSON/emocloche") as TextAsset;
+        data = JsonUtility.FromJson<Chart>(loadedJson.ToString());
+        SetUI();
+        Invoke("setBgm", 5f);
+        InvokeRepeating("MakeNote", 5f - 0.5f, 60f / (float)data.bpm);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        UI.transform.Find("FPS").GetComponent<TextMeshProUGUI>().text = "FPS " + (1.0f / deltaTime).ToString();
     }
 }
--- Assets/note/NoteParent.cs
+++ Assets/note/NoteParent.cs
@@ -3,26 +3,16 @@ using UnityEngine;
 
 public class NoteParent : MonoBehaviour
 {
-    public Rigidbody note;
     public int dir;
 
     void Start()
     {
-        note = GetComponent<Rigidbody>();
-        float x = Mathf.Sin(dir * Mathf.Deg2Rad) * 0.05f;
-        float y = Mathf.Cos(dir * Mathf.Deg2Rad) * 0.05f;
-        //note.velocity = new Vector3(x, y, 0);
-
-        transform.localScale = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(0f, 0f, 0f);
         transform.Rotate(0f, 0f, dir, Space.Self);
     }
 
-    void Update() {
-        transform.localScale = new Vector3(transform.localScale.x + 0.1f, transform.localScale.y + 0.1f, transform.localScale.z + 0.1f);
-    }
-
-    void OnBecameInvisible()
+    void Update()
     {
-        Destroy(gameObject);
+        transform.localScale = new Vector3(transform.localScale.x + 0.05f, transform.localScale.y + 0.05f, transform.localScale.z + 0.05f);
     }
 }
