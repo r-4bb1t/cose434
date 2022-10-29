34f952197a2a630fb4685699d185af3c133e49aa
havana723 <jeeen1>
2022-10-29 15:01:18 +0900

    fix

--- Assets/TouchscreenController.cs
+++ Assets/TouchscreenController.cs
@@ -7,49 +7,26 @@ public class TouchscreenController : MonoBehaviour
     public GameObject fingertip;
     public GameObject game;
     public GameObject ui;
-    bool isPointing;
-    bool isTouching;
     float touchDistance;
     Vector3 FingertipForward;
     void Start()
     {
         FingertipForward = fingertip.transform.TransformDirection(Vector3.forward);
-        touchDistance = 0.005f;
-        isTouching = false;
-        isPointing = false;
+        touchDistance = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckPointing();
-
         if (Physics.Raycast(fingertip.transform.position, FingertipForward, out RaycastHit ray, touchDistance))
         {
             Collider rayCollider = ray.collider;
             if (rayCollider.gameObject.name.Equals("Emocloche"))
             {
-                isTouching = true;
-                rayCollider.gameObject.GetComponent<GameObject>().transform.Find("Light").GetComponent<Light>().enabled = true;
-                game.GetComponent<GameObject>().SetActive(true);
-                ui.GetComponent<GameObject>().SetActive(false);
+                rayCollider.gameObject.transform.Find("Light").GetComponent<Light>().enabled = true;
+                game.SetActive(true);
+                ui.SetActive(false);
             }
-            else
-            {
-                isTouching = false;
-            }
-        }
-    }
-
-    void CheckPointing()
-    {
-        if (!OVRInput.Get(OVRInput.NearTouch.SecondaryIndexTrigger))
-        {
-            isPointing = true;
-        }
-        else
-        {
-            isPointing = false;
         }
     }
 }
