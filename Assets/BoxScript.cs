0d0cdc05c52a5e80ed83efe8cf50d7b4c5234e17
r4bb1t <r4bb1t.github@gmail.com>
2022-10-29 19:06:04 +0900

    find closest note

--- Assets/BoxScript.cs
+++ Assets/BoxScript.cs
@@ -15,13 +15,29 @@ public class BoxScript : MonoBehaviour
 
     }
 
+    GameObject FindClosestNote()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Note");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
     public void Trigger()
     {
-        GameObject[] notes = GameObject.FindGameObjectsWithTag("Note");
-        if (notes.Length > 0)
-        {
-            Debug.Log(notes[0].gameObject);
-            Destroy(notes[0].gameObject);
-        }
+        Debug.Log(FindClosestNote());
+        Destroy(FindClosestNote());
     }
 }
